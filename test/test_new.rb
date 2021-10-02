42ecf94603f9d034d73a614d74cb748582c97333
Afr Schoe <58883403+q9f@users.noreply.github.com>
2021-10-02 15:27:40 +0200

    ext: more strict c compiler
    Co-authored-by: Alex Kotov <kotovalexarian@gmail.com>

---
+++ test/test_new.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'test/unit'
+
+class SHA3NewTests < Test::Unit::TestCase
+  def test_singleton_method_hexdigest_256_empty
+    result = Digest::SHA3.hexdigest '', 256
+    assert_instance_of String, result
+    assert_equal 'c5d2460186f7233c927e7db2dcc703c0' \
+                 'e500b653ca82273b7bfad8045d85a470',
+                 result
+  end
+
+  def test_singleton_method_hexdigest_256_sample
+    result = Digest::SHA3.hexdigest 'sample', 256
+    assert_instance_of String, result
+    assert_equal 'b80204f7e9243e4fca5489740ccd31dc' \
+                 'd0a54619a7f4165cee73c191ef7271a1',
+                 result
+  end
+end
--- test/test_usage.rb
+++ test/test_usage.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'test/unit'
 
 class SHA3UsageTest < Test::Unit::TestCase
