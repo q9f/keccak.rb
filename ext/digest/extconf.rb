9e30bc74ec0db57a5dd7106c067627a869132a00
httpsx <httpsx1@gmail.com>
2022-08-22 05:44:33 +0500

    Fix install on Windows

--- ext/digest/extconf.rb
+++ ext/digest/extconf.rb
@@ -5,7 +5,7 @@ require 'mkmf'
 
 def cflags(*args)
   args.each do |str|
-    $CFLAGS += (ENV['OS'] == "Windows_NT") ? " #{str}" : " #{str.shellescape}" 
+    $CFLAGS += (ENV['OS'] == "Windows_NT") ? " #{str} " : " #{str.shellescape} " 
   end
 end
 
